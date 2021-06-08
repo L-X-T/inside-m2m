cff0069746f6d7565fe9a6b77f7325dae497b090
Alex Thalhammer <alex@angular-software.at>
2021-06-08 18:07:53 +0200

    Add preloading

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 import { FlightLibModule } from '@flight-workspace/flight-lib';
 
 import { AppComponent } from './app.component';
@@ -26,7 +26,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
     FlightLibModule.forRoot(),
     SharedModule.forRoot(),
-    RouterModule.forRoot(APP_ROUTES, { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(APP_ROUTES, { relativeLinkResolution: 'legacy', preloadingStrategy: PreloadAllModules })
   ],
   declarations: [AppComponent, SidebarComponent, NavbarComponent, HomeComponent, BasketComponent],
   providers: [],
