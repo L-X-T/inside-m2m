e50d6e8307424dc23788c2a24f9ffb115a1e89f6
Alex Thalhammer <alex@angular-software.at>
2021-06-08 15:49:51 +0200

    Bonus: Share a Library of Your Monorepo

--- module-federation/projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
+++ module-federation/projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
@@ -1,4 +1,5 @@
 import {Component, ViewChild, ViewContainerRef, Inject, Injector, ComponentFactoryResolver, OnInit} from '@angular/core';
+import { AuthLibService } from 'auth-lib';
 
 @Component({
   selector: 'app-flights-search',
@@ -8,8 +9,10 @@ export class FlightsSearchComponent {
 
   @ViewChild('vc', { read: ViewContainerRef, static: true })
   viewContainer: ViewContainerRef;
+  user = this.authLibService.user;
 
   constructor(
+    private authLibService: AuthLibService,
     @Inject(Injector) private injector,
     @Inject(ComponentFactoryResolver) private cfr) { }
 
